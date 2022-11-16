a45a1f44ba4fb3d5bb757639e16c5339aaca68cc
Przemysław Tracz <przmktrc@gmail.com>
2022-11-16 20:30:34 +0100

    Added Printer

--- src/test_sio2.py
+++ src/test_sio2.py
@@ -9,9 +9,47 @@ from typing import cast
 
 
 
-def verbose_print(message: str) -> None:
-    if config.verbose:
+class Printer():
+    current_indent: int = 0
+    spaces_per_indent: int = 3
+    is_print_pending: bool = False
+
+    def verbose_print(self, message: str) -> "Printer":
+        if config.verbose:
+            self.always_print(message)
+        return self
+
+    def always_print(self, message: str) -> "Printer":
+        self.print_newline_if_pending()
+        print((" " * self.current_indent * self.spaces_per_indent) + message)
+        return self
+
+    def indent(self) -> "Printer":
+        self.current_indent += 1
+        return self
+
+    def deindent(self) -> "Printer":
+        self.current_indent -= 1
+        return self
+
+    def print_newline_if_pending(self) -> None:
+        if self.is_print_pending:
+            print("")
+            self.is_print_pending = False
+
+    def start_pending_print(self, message: str) -> "Printer":
+        print(message, flush=True, end="")
+        self.is_print_pending = True
+        return self
+
+    def end_pending_print(self, message: str) -> "Printer":
         print(message)
+        self.is_print_pending = False
+        return self
+
+
+
+printer = Printer()
 
 
 
@@ -29,9 +67,8 @@ class Tester():
     def test_directory(self, test_dir: str) -> Result:
         number_correct = number_total = 0
 
-        print("Testing directory: \"{}\"".format(get_last_part(test_dir)).ljust(45),
-              flush=True,
-              end="")
+        printer.start_pending_print("Testing directory: \"{}\"".format(
+            get_last_part(test_dir)).ljust(45)).indent()
 
         in_dir = test_dir + "/in/"
 
@@ -45,7 +82,7 @@ class Tester():
             if is_correct:
                 number_correct += 1
 
-        print("OK" if number_total == number_correct else "WRONG")
+        printer.deindent().end_pending_print("OK" if number_total == number_correct else "WRONG")
 
         return (get_last_part(test_dir), number_total, number_correct)
 
@@ -57,7 +94,7 @@ class Tester():
         in_file = test_dir + "/in/" + in_file
 
         if not is_valid_path(out_file):
-            print(f"   File \"{in_last_part}\"".ljust(30) + "MISSING_OUT")
+            printer.always_print("File \"{}\"".format(in_last_part).ljust(30) + "MISSING_OUT")
 
         # print("   > Command: \"{}\"".format(
         #     config.run_exec_cmd.format(exec=remove_blank_whitespaces(config.exec_path),
@@ -72,9 +109,9 @@ class Tester():
         is_correct = self.is_correct(out_file, temp_file)
 
         if is_correct:
-            verbose_print(f"   File \"{in_last_part}\"".ljust(30) + "OK")
+            printer.verbose_print("File \"{}\"".format(in_last_part).ljust(30) + "OK")
         else:
-            print(f"   File \"{in_last_part}\"".ljust(30) + "WRONG")
+            printer.always_print("File \"{}\"".format(in_last_part).ljust(30) + "WRONG")
 
         return is_correct
 
@@ -88,10 +125,10 @@ class Tester():
             return filecmp.cmp(out_file, temp_file)
 
     def print_results(self) -> None:
-        print("Result:")
+        printer.always_print("Result:").indent()
         for result in self.results:
-            print(
-                f"   Test dir \"{result[0]}\"".ljust(40) +
+            printer.always_print(
+                f"Test dir \"{result[0]}\"".ljust(40) +
                 f"TOTAL: {result[1]:>4}   CORRECT: {result[2]:>4}   WRONG: {result[1] - result[2]:>4}"
             )
 
